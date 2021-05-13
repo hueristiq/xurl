7526103250ecef33fedd03f67ab7319317d99764
Alex Munene <62714471+enenumxela@users.noreply.github.com>
2021-05-13 09:41:03 +0300

    feat(parsing): Add extra fields to urlx.URL
    Add fields: Host (e.g. sub.example.com), RootDomain (e.g. example),
    SubDOmain (e.g. sub), TLD (e.g. com), Port (e.g. 80) - to extract
    the respective bits from a url.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/enenumxela/urlx
 
 go 1.15
+
+require golang.org/x/net v0.0.0-20210510120150-4163338589ed
--- pkg/urlx/urlx.go
+++ pkg/urlx/urlx.go
@@ -1,18 +1,49 @@
 package urlx
 
-import "net/url"
+import (
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/publicsuffix"
+)
 
 type URL struct {
+	// scheme, user (username & password), host (host or host:Port), path, query, fragment
 	*url.URL
+	// Host (e.g. sub.example.com), RootDomain (e.g. example), SubDOmain (e.g. sub), TLD (e.g. com), Port (e.g. 80)
+	Domain, RootDomain, SubDomain, TLD, Port string
 }
 
-func Parse(rawURL string) (*URL, error) {
-	parsedURL, err := url.Parse(rawURL)
+func Parse(rawURL string) (parsedURL *URL, err error) {
+	parsedURL = &URL{}
+
+	parsedURL.URL, err = url.Parse(rawURL)
+	if err != nil {
+		return
+	}
+
+	for i := len(parsedURL.URL.Host) - 1; i >= 0; i-- {
+		if parsedURL.URL.Host[i] == ':' {
+			parsedURL.Domain = parsedURL.URL.Host[:i]
+			parsedURL.Port = parsedURL.URL.Host[i+1:]
+			break
+		} else if parsedURL.URL.Host[i] < '0' || parsedURL.URL.Host[i] > '9' {
+			parsedURL.Domain = parsedURL.URL.Host
+		}
+	}
+
+	etldPlus1, err := publicsuffix.EffectiveTLDPlusOne(parsedURL.Domain)
 	if err != nil {
 		return nil, err
 	}
 
-	return &URL{
-		URL: parsedURL,
-	}, nil
+	i := strings.Index(etldPlus1, ".")
+	parsedURL.RootDomain = etldPlus1[0:i]
+	parsedURL.TLD = etldPlus1[i+1:]
+
+	if rest := strings.TrimSuffix(parsedURL.Domain, "."+etldPlus1); rest != parsedURL.Domain {
+		parsedURL.SubDomain = rest
+	}
+
+	return
 }
